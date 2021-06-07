b8a895ae041f8907c3eb616c96708a274ae22f85
Mark Alvin Tayag <mtayag@salesforce.com>
2021-06-07 20:30:13 +0800

    Reserved Words

--- src/jsonapexgeneratorcore/Helper/Helper.cs
+++ src/jsonapexgeneratorcore/Helper/Helper.cs
@@ -6,6 +6,28 @@ using System.Text.Json.Serialization;
 public static class Helper {
     //JSON to Variable Conversion
     //Handle Reserved Words
+    private static List<String> reserved = new List<String>() { "abstract", "activate", "and", "any", "array", "as", "asc", "autonomous", 
+			"begin", "bigdecimal", "blob", "boolean", "break", "bulk", "by", "byte", 
+			"case", "cast", "catch", "char", "class", "collect", "commit", "const", "continue", "currency", 
+			"date", "datetime", "decimal", "default", "delete", "desc", "do", "double", 
+			"else", "end", "enum", "exception", "exit", "export", "extends", 
+			"false", "final", "finally", "float", "for", "from", 
+			"global", "goto", "group", 
+			"having", "hint", 
+			"if", "implements", "import", "in", "inner", "insert", "instanceof", "int", "integer", "interface", "into", 
+			"join", 
+			"like", "limit", "list", "long", "loop", 
+			"map", "merge", 
+			"new", "not", "null", "nulls", "number", 
+			"object", "of", "on", "or", "outer", "override", 
+			"package", "parallel", "pragma", "private", "protected", "public", 
+			"retrieve", "return", "rollback", 
+			"select", "set", "short", "sObject", "sort", "static", "string", "super", "switch", "synchronized", "system", 
+			"testmethod", "then", "this", "throw", "time", "transaction", "trigger", "true", "try", 
+			"undelete", "update", "upsert", "using", 
+			"virtual", "void", 
+			"webservice", "when", "where", "while" };
+
     //Handle Sub Branches
     public static List<String> jsonToClass(String jsonStr) {
         List<String> lstVars = new List<String>();
@@ -23,12 +45,17 @@ public static class Helper {
     //This will keep the Mapping between JSONType and Apex Primitives
     private static String parseVarType (JsonProperty property) {
         String apexType = "";
+        String propertyName = property.Name;
+        if (reserved.Contains(propertyName)) {
+            propertyName = "_" + propertyName;
+        }
+
         switch (property.GetType().FullName) {
             case "String":
-                apexType = "String " + property.Name + " { get; set; }";
+                apexType = "String " + propertyName + " { get; set; }";
             break;
             case "Int32":
-                apexType = "Integer " + property.Name + " { get; set; }";
+                apexType = "Integer " + propertyName + " { get; set; }";
             break;
             //Handle Subobjects
             //Handle Arrays
